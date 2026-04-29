b272a7db6bb4236da639e9a7200e692d446e7c81
naeelhk <naeelhk@localhost.local>
2026-04-29 11:43:43 +0200

    Création du gitignore

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install.sh
+# Script d'installation de ADBricks
+# 
+# Auteur    :   NaelH
+# Version   :   1.0.0
+# Usage     :   sudo ./install.sh
+#
+# Dépendance : 
+#   -   Internet
+#   -   sudo
+#
+# Journal de bord :
+#  29th april 2026 : Beginning of the project 
